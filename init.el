3dee51e2558f8c994d080fba4f05169e8caee987
unknown <matsuura@.acr.atr.jp>
2010-08-05 16:07:54 +0900

    commit

---
+++ init.el
@@ -0,0 +1,8 @@
+(set-face-attribute 'default nil
+        :family "‚l‚r ƒSƒVƒbƒN"
+        :height 90)
+(set-fontset-font "fontset-default"
+        'japanese-jisx0208
+        '("‚l‚r ƒSƒVƒbƒN" . "jisx0208- sjis"))
+
+
